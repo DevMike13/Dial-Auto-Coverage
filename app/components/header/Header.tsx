292ac860de46fe3a8c3ade52d619e8f157dc29b1
Mike Gerard Suico <mgsuico.mentorspire@gmail.com>
2025-04-24 12:24:23 +0800

    Changed logo

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className='w-full h-[90px] bg-[#e8f1ff] border-gray-300 border-b-1 flex items-center justify-between px-2 md:px-12'>
       <div>
-        <img src="/logo.png" alt="Logo" width={280} height={101} className='hidden sm:block' />
-        <img src="/logo_m.png" alt="Logo" width={50} height={50} className='block sm:hidden' />
+        <img src="/DAC_logo_1.png" alt="Logo" width={280} height={101} className='hidden sm:block' />
+        <img src="/DAC_logo_1_M.png" alt="Logo" width={50} height={50} className='block sm:hidden' />
       </div>
       <div className='flex items-center justify-center md:gap-5 gap-2'>
         <img src="/ph-call-icn.png" alt="Logo" width={42} height={42} className='hidden sm:block animate-tada' />
